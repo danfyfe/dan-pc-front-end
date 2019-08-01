e341f9d5d8104731eade1281becbfe9ebe138363
danfyfe <fyfedaniel@gmail.com>
2019-08-01 17:28:50 -0400

    master section start

--- src/components/MasterChannel.js
+++ src/components/MasterChannel.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import MasterMetersContainer from '../containers/MasterMetersContainer.js'
+import MasterEqualizersContainer from '../containers/MasterEqualizersContainer.js'
 
 function MasterChannel() {
   return(
     <div className='master-channel'>
       <MasterMetersContainer/>
+      <MasterEqualizersContainer/>
     </div>
   )
 }
---
+++ src/containers/MasterEqualizersContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+
+function MasterEqualizersContainer() {
+  return(
+    <div className='master-eq-container'>
+    </div>
+  )
+}
+
+export default MasterEqualizersContainer
